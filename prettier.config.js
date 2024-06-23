4fa5bf4959e870798428c3a27bcc62aa31b6b9c1
Tobbe Lundberg <tobbe@tlundberg.com>
2024-06-23 06:59:08 -0700

    Upgrade to prettier v3

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,12 @@
 // https://prettier.io/docs/en/options.html
-/** @type {import('prettier').RequiredOptions} */
-module.exports = {
+/** @type {import('prettier').Options} */
+const config = {
   bracketSpacing: true,
   tabWidth: 2,
   semi: false,
   singleQuote: true,
 }
+
+// Need a separate object (`config` in this case) to work around this issue:
+// https://github.com/microsoft/TypeScript/issues/47107
+module.exports = config
