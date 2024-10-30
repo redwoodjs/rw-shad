80e757a96a0543fe26ad0ac69b66bcbb0b843275
Tobbe Lundberg <tobbe@tlundberg.com>
2024-10-30 15:11:25 +0100

    fix(tag): Use correct tag name for rw-shad releases

--- packages/add/tag.ts
+++ packages/add/tag.ts
@@ -5,13 +5,13 @@ import execa from 'execa'
 const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
 
 const { stdout: stdoutCommit } = await execa.command(
-  `git commit -am setup-rw-shad/v${packageJson.version}`,
+  `git commit -am rw-shad/v${packageJson.version}`,
 )
 
 console.log(stdoutCommit)
 
 const { stdout: stdoutTag } = await execa.command(
-  `git tag setup-rw-shad/v${packageJson.version}`,
+  `git tag rw-shad/v${packageJson.version}`,
 )
 
 console.log(stdoutTag)
